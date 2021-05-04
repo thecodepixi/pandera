b284300d9ac4a7e259036d1bcb17a336bd04504b
Josh Barnett <oh@heyjo.sh>
2021-05-03 22:44:06 -0400

    Added config files for ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": "warn",
+  },
+  settings: {
+    usePrettierrc: true,
+  },
+};
